756f9f9c4234a77db26a170474823d333e9b234a
Ny Eja Asaramanitra <razanatsimbanyeja@gmail.com>
2025-07-07 17:09:04 +0300

    client tohiny
    client tohiny

--- ws/index.php
+++ ws/index.php
@@ -5,6 +5,6 @@ require 'routes/etudiant_routes.php';
 require 'routes/pret_routes.php';
 require 'routes/login_routes.php';
 require 'routes/typePret_routes.php';
-
+require 'routes/client_routes.php';
 
 Flight::start();
