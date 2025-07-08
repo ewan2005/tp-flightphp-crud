b925f75bcd37c23a0bae5dbb3a0da35a064f72ca
ewan2005 <ewanjudiotsarazaka@gmail.com>
2025-07-08 13:48:47 +0300

    update-db
    update-db

--- ws/db.php
+++ ws/db.php
@@ -1,9 +1,15 @@
 <?php
+/* $bdd = mysqli_connect('172.80.237.54', 'ETU003389', 'u803RF8V', 'db_s2_ETU003389');*/
 function getDB() {
     $host = 'localhost';
     $dbname = 'etablissement';
     $username = 'root';
     $password = '';
+
+    // $host = 'localhost';
+    // $dbname = 'db_s2_ETU003389';
+    // $username = 'ETU003389';
+    // $password = 'u803RF8V';
 
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
